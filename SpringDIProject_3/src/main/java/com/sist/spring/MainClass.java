1b64a401b7f98f3662a3a68de58856219a62e802
sist-106 <sist-106@DESKTOP-RE7C273>
2025-03-18 14:26:17 +0900

    2025-03-18 Spring / DI

---
+++ SpringDIProject_3/src/main/java/com/sist/spring/MainClass.java
@@ -0,0 +1,28 @@
+package com.sist.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// String[] xml={"app-member.xml", "app-sawon.xml", "app-student.xml"};
+		ApplicationContext app=new ClassPathXmlApplicationContext("app-*.xml");
+		Sawon sa=(Sawon)app.getBean("sa");
+		System.out.println(sa.getSabun());
+		System.out.println(sa.getName());
+		System.out.println(sa.getDept());
+		System.out.println("=================");
+		Member mem=(Member)app.getBean("mem");
+		System.out.println(mem.getMno());
+		System.out.println(mem.getName());
+		System.out.println(mem.getAddress());
+		System.out.println("=================");
+		Student std=(Student)app.getBean("std");
+		System.out.println(std.getHakbun());
+		System.out.println(std.getName());
+		System.out.println(std.getSubject());
+	}
+
+}
---
+++ SpringDIProject_3/src/main/java/com/sist/spring/Sawon.java
@@ -0,0 +1,10 @@
+package com.sist.spring;
+
+import lombok.Data;
+
+@Data
+public class Sawon {
+	private int sabun;
+	private String name;
+	private String dept;
+}
