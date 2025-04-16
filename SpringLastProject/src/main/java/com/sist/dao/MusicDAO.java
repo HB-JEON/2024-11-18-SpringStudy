5d16ef09266ea726d13c796f5169792120a6f028
HB-JEON <wjsgusqls@nate.com>
2025-04-16 17:17:21 +0900

    2025-04-16 key 값 받아오기 및 출력

---
+++ SpringLastProject/src/main/java/com/sist/dao/MusicDAO.java
@@ -0,0 +1,30 @@
+package com.sist.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import java.util.*;
+import com.sist.vo.*;
+import com.sist.mapper.*;
+
+@Repository
+public class MusicDAO {
+	@Autowired
+	private MusicMapper mapper;
+	
+	public void musicInsert(MusicVO vo)
+	{
+		mapper.musicInsert(vo);
+	}
+	public void musicDelete()
+	{
+		mapper.musicDelete();
+	}
+	public MusicVO musicTop() 
+	{
+		return mapper.musicTop();
+	}
+	public void allDelete(int empno)
+	{
+		mapper.allDelete(empno);
+	}
+}
