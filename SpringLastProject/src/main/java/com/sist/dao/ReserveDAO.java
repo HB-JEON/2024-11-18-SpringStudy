87ff5e912e6d940ae51318f210c149d713b240e1
HB-JEON <wjsgusqls@nate.com>
2025-04-22 15:04:33 +0900

    2025-04-22 예약 CRUD

---
+++ SpringLastProject/src/main/java/com/sist/dao/ReserveDAO.java
@@ -0,0 +1,38 @@
+package com.sist.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import java.util.*;
+import com.sist.mapper.*;
+import com.sist.vo.FoodVO;
+import com.sist.vo.ReserveVO;
+@Repository
+public class ReserveDAO {
+	@Autowired
+	private ReserveMapper mapper;
+	
+	public List<FoodVO> busanFoodReserveData()
+	{
+		return mapper.busanFoodReserveData();
+	}
+	public void reserveInsert(ReserveVO vo)
+	{
+		mapper.reserveInsert(vo);
+	}
+	public List<ReserveVO> reservoMyPageListData(String userid)
+	{
+		return mapper.reserveMyPageListData(userid);
+	}
+	public List<ReserveVO> reserveAdminPageListData()
+	{
+		return mapper.reserveAdminPageListData();
+	}
+	public void reserveDelete(int rno)
+	{
+		mapper.reserveDelete(rno);
+	}
+	public void reserveUpdate(int rno)
+	{
+		mapper.reserveUpdate(rno);
+	}
+}
--- SpringLastProject/src/main/java/com/sist/vo/ReserveVO.java
+++ SpringLastProject/src/main/java/com/sist/vo/ReserveVO.java
@@ -1,5 +1,13 @@
 package com.sist.vo;
 
-public class ReserveVO {
+import lombok.Data;
+import java.util.*;
 
+@Data
+public class ReserveVO {
+	private int rno, fno, isReserve;
+	private String userid, rday, rtime, rinwon, dbday;
+	private Date regday;
+	private FoodVO fvo=new FoodVO();
+	private MemberVO mvo=new MemberVO();
 }
